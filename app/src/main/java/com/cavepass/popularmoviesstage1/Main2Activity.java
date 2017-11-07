06b941ddc93d7b6459a8bf73492ce84d135a917d
ajay1271 <ajayanurag.05@gmail.com>
2017-11-07 21:10:30 +0530

    Update Main2Activity.java

--- app/src/main/java/com/cavepass/popularmoviesstage1/Main2Activity.java
+++ app/src/main/java/com/cavepass/popularmoviesstage1/Main2Activity.java
@@ -3,11 +3,13 @@ package com.cavepass.popularmoviesstage1;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import org.json.*;
 import java.io.BufferedReader;
@@ -19,6 +21,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
@@ -27,7 +31,7 @@ public class Main2Activity extends AppCompatActivity {
     ArrayList<cards> movies = new ArrayList<>();
 
 
-    private GridView gridview;
+    private GridView listView;
 
 
 
@@ -42,12 +46,12 @@ public class Main2Activity extends AppCompatActivity {
         {
 
 
-            gridview = (GridView) findViewById(R.id.grid);
-            gridview.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
+            listView = (GridView) findViewById(R.id.grid);
+
 
             MovieAsyncTask task = new MovieAsyncTask();
             task.execute();
-            gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> a, View v, int position,
                                         long id) {
@@ -74,22 +78,53 @@ public class Main2Activity extends AppCompatActivity {
         }
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mymenu, menu);
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.action_bar_spinner, menu);
+
+        MenuItem item = menu.findItem(R.id.spinner);
+        final Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_list_item_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+                String items = spinner.getSelectedItem().toString();
+
+
+                if(items.equals(getString(R.string.popularity))){
+
+                    Intent j = new Intent(Main2Activity.this,MainActivity.class);
+
+                    startActivity(j);
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+
+        });
+
+
+
+
+        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent i = new Intent(Main2Activity.this,sort.class);
-        startActivity(i);
-
-
-
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 
@@ -217,7 +252,7 @@ public class Main2Activity extends AppCompatActivity {
 
 
 
-                gridview.setAdapter(movieArrayAdapter);
+                listView.setAdapter(movieArrayAdapter);
 
 
 
